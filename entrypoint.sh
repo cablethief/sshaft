8b2b39a30e818e24d10bc12999badd9be82b772f
Cablethief <mikek3262@gmail.com>
2019-09-20 21:58:33 +0200

    Pubkeyauth enable

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,6 +44,7 @@ fi
 if [[ -n "${ROOT_SSHKEY}" ]] ; then
     echo "Root user enabled, using the key: ${ROOT_SSHKEY}."
     sed -i "s/#PermitRootLogin.*/PermitRootLogin prohibit-password/" /etc/ssh/sshd_config
+    sed -i "s/#PubkeyAuthentication.*/PubkeyAuthentication yes/" /etc/ssh/sshd_config
     mkdir /root/.ssh
     echo "${ROOT_SSHKEY}" >> /root/.ssh/authorized_keys
 fi
